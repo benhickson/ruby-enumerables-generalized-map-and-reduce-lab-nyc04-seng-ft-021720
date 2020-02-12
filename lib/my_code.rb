b7f515c0aa6897f94b80f58175543c88484a80f9
Ben Hickson <bzhickson@gmail.com>
2020-02-12 00:55:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,49 @@
-# Your Code Here
+
+
+# my_array = [1,2,3]
+
+# my_array.map do |num|
+#   num + 1
+# end
+
+
+# my_array.map {|num| num + 1}
+
+
+# def add_numbers(num1, num2)
+#   total = num1 + num2
+#   yield(total)
+#   total
+# end
+
+# myvar = add_numbers(1,2) do |t|
+#   puts "The total is #{t}"
+# end
+
+# add_numbers(2,5) do |t|
+#   puts "That adds up to #{t}"
+# end
+
+# puts myvar
+
+
+
+def map(array)
+  new = []
+  i = 0
+  while i < array.length
+    new.push(yield(array[i]))
+    i += 1
+  end
+  new
+end
+
+def reduce(array, starting_value = 0)
+  
+  output = starting_value
+
+  array.length.times do |i|
+    output = yield(output, array[i])
+  end
+  output
+end
